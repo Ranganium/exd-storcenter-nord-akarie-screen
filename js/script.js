b96049d50b44f77b16654465c76a8af54cbb24a9
Mariadl0906 <eaa25mdla@students.eaaa.dk>
2025-11-11 12:10:25 +0100

    Bobler er tilføjet :))))

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ if (akvarieMandenClosedImg) {
        }, 10000);
     });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const bubblesContainer = document.querySelector('.bubbles');
+  for (let i = 0; i < 18; i++) {
+    const bubble = document.createElement('div');
+    bubble.className = 'bubble';
+
+    // Generer en tilfældig position for boblen horisontalt
+    const randomLeft = Math.random() * 100; // 0 til 100%
+    bubble.style.left = `${randomLeft}vw`; // Sæt venstre position i vw
+
+    // Generer en tilfældig startposition vertikalt
+    const randomBottom = Math.random() * 80; // 0 til 80vh
+    bubble.style.bottom = `${randomBottom}vh`; // Sæt bundt position i vh
+
+    bubblesContainer.appendChild(bubble);
+  }
+});
